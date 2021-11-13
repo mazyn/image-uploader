7e6fa2e8051d79afc7101056e125497310de6847
mazyn <gilmarfilho75@gmail.com>
2021-11-13 18:04:48 -0300

    feat(frontend): :sparkles: initial behavior of droppable component

--- frontend/components/DndUploader.tsx
+++ frontend/components/DndUploader.tsx
@@ -1,21 +1,77 @@
-import Image from 'next/image'
-import styles from './DndUploader.module.css'
-import imagePlaceholder from '../public/image.svg'
-import Button from './Button'
+import Image from 'next/image';
+import styles from './DndUploader.module.css';
+import imagePlaceholder from '../public/image.svg';
+import Button from './Button';
+import { DragEventHandler, useState } from 'react';
 
 const DndUploader: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const dragOver: DragEventHandler<HTMLDivElement> = e => {
+    e.preventDefault();
+  };
+
+  const dragEnter: DragEventHandler<HTMLDivElement> = e => {
+    e.preventDefault();
+  };
+
+  const dragLeave: DragEventHandler<HTMLDivElement> = e => {
+    e.preventDefault();
+  };
+
+  const fileDrop: DragEventHandler<HTMLDivElement> = e => {
+    e.preventDefault();
+    const files = e.dataTransfer?.files;
+    console.log({ files });
+    if (files.length) {
+      handleFile(files[0]);
+    }
+  };
+
+  const handleFile = (file: File) => {
+    if (!validateFile(file)) {
+      setErrorMessage('File type not allowed.');
+      return;
+    }
+
+    setErrorMessage('');
+    setSelectedFile(file);
+  };
+
+  const validateFile = (file: File) => {
+    const validTypes = [
+      'image/jpeg',
+      'image/jpg',
+      'image/png',
+      'image/gif',
+      'image/x-icon',
+    ];
+
+    return validTypes.indexOf(file.type) != -1;
+  };
+
   return (
     <>
-      <div className={styles.dndContainer}>
+      <div
+        className={styles.dropContainer}
+        onDragOver={dragOver}
+        onDragEnter={dragEnter}
+        onDragLeave={dragLeave}
+        onDrop={fileDrop}
+      >
         <div className={styles.imageWrapper}>
           <Image src={imagePlaceholder} alt='Upload a photo' />
         </div>
         <legend className={styles.legend}>Drag & Drop your image here</legend>
       </div>
+      {errorMessage && (
+        <span className={styles.errorMessage}>{errorMessage}</span>
+      )}
       <span className={styles.or}>Or</span>
       <Button label='Choose a file' action={() => {}} />
     </>
-  )
-}
+  );
+};
 
-export default DndUploader
+export default DndUploader;
